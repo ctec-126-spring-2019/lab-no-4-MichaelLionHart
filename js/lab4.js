bacab3a9e15e1af6514f0fb0d18027532fb0312a
The Mike <m.hart@students.clark.edu>
2019-04-20 21:15:00 -0700

    Lab 4 Updates

--- js/lab4.js
+++ js/lab4.js
@@ -3,28 +3,52 @@
 
 function verse1(){
     let output = '';
-    // your code here
+    output = `<p>I get on with life as a doctor, <br>
+    I'm a funny kinda person. <br>
+    I like programming on Sundays, <br>
+    I like staring at walls in the week. <br>
+    I like to contemplate pizza. <br>
+    But when I start to daydream, <br>
+    My mind turns straight to root beer.</p>`;
 
     return output;
 }
 
 function verse2(){
     let output = '';
-    // your code here
+    output = `<p>I like to use words like 'crikey,'<br>
+    I like to use words like 'super.'<br>
+    I like to use words about pizza.<br>
+    But when I stop my talking,<br>
+    My mind turns straight to root beer.</p>`;
 
     return output;
 }
 
-function chorus1(){
+function verse3(){
     let output = '';
-    // your code here
+    output = `<p>I like to hang out with Mitch,<br>
+    I like to kick back with Lunch Lady,<br>
+    But when left alone,<br>
+    My mind turns straight to root beer.</p>`;
 
     return output;
 }
 
-function chorus2(){
+function verse4(){
     let output = '';
-    // your code here
+    output = `<p>I'm not too fond of leprechauns,<br>
+    I really hate ghosts,<br>
+    But I just think back to root beer,<br>
+    And I'm happy once again.</p>`;
+
+    return output;
+}
+
+function chorus(){
+    let output = '';
+    output = `<p>Do I love root beer more than pizza? <br>
+    Do I love root beer more than pizza?</p>`;
 
     return output;
 }
@@ -34,6 +58,14 @@ function chorus2(){
 function main(){
     let finalOutput = ''; // used to build lyrics string
     // call verse and chorus functions
+    finalOutput = `${verse1()}
+                   ${chorus()}
+                   ${verse2()}
+                   ${chorus()}
+                   ${verse3()}
+                   ${chorus()}
+                   ${verse4()}
+                   ${chorus()}`;
     // each verse/chorus should return text including HTML paragraph tags
 
     // do not modify the statement below
